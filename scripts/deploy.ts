6fe57e01e633606409625c9dc113ea21fbf57168
Ternder001 <nicholas.akura@gmail.com>
2024-02-13 23:43:00 +0100

    deploy script added

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,23 @@
-import { ethers } from "hardhat";
+// scripts/deploy.ts
+import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log('Deploying contracts with the account:', deployer.address);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const SaveEther = await ethers.getContractFactory('SaveEther');
+  const saveEther = await SaveEther.deploy();
+  
+  await saveEther.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log('SaveEther deployed to:', saveEther.target);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
